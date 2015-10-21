9894fd1883a8385e6e5917400b495cc40c3418fd
Alex Irizarry <itsmealex79@gmail.com>
2015-10-21 01:30:16 -0400

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Game\Config\Settings;
+use Game\Cards\Deck;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+
+// Create new card deck. 
+$game = new Deck;
+$deck = $game->getNewDeck(Settings::$URL);
+
+// Routes
+$app->get('/deck', function() use ($deck) {
+    return $deck;
+});
+
+$app->run();
